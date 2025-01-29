7175462b26d52cced79c36ee1a38e6d3ac4cbcb3
lynn-sketch <110109942+lynn-sketch@users.noreply.github.com>
2025-01-29 16:11:44 +0300

    Add files via upload

--- DiamondDatasetPractice.R
+++ DiamondDatasetPractice.R
@@ -1,15 +1,80 @@
 ##SETTING WORKING DIRECTORATE
 setwd("C:/Users/HP PC/Desktop/BSDS 2_2/BIG DATA ANALYTICS")
 
-##Calling the libraries to be used
-library(tidyverse)
-library(tidyr)
-library(ggplot2)
-library(readr)
-library(readxl)
-library(tidyselect)
 
-df <- read.csv("C:/Users/HP PC/Desktop/BSDS 2_2/BIG DATA ANALYTICS/class Discussion/class Discussion/Diamond dataset/diamonds.csv")
+#Preprocessing ----
+
+##Loading libraries----
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+
+##Loading data
+data <- read.csv("C:/Users/HP PC/Desktop/BSDS 2_2/BIG DATA ANALYTICS/class Discussion/class Discussion/Diamond dataset/diamonds.csv")
+head(data) ##A review of first  6 columns              
+view(data)
+
+
+#Structure of data
+str(data)
+
+#Summarize stats
+summary(data)
+
+##Handling missing values----
+##Approach 1
+
+missing_values <- sapply(data, function(x) sum(is.na(x)))
+missing_values
+
+##Approach 2
+missing_values_two <- data %>% summarise(numeric_missing = sum(is.na(.)),
+                                         categoric_missing = sum(is.na(as.character(.))))
+missing_values_two
+
+##Approach three
+missing_values_three <- data %>%  summarise_all(funs(sum(is.na(.))))
+missing_values_three
+
+##Removing Missing values----
+
+##Approach one
+##Dropping methods
+
+new_data = na.omit(data)
+
+
+##Imputing data
+class(data$color)
+class(data$depth)
+
+hist(data$depth,col = "blue", main ="Histogram for depth")
+
+##Impute for missing values
+impute_mean <- function(x) replace(x, is.na(x), mean(x, na.rm = TRUE)) ##Normalize numerical data
+impute_median <- function(x) replace(x, is.na(x), median(x, na.rm = TRUE)) ##Skewed numerical data
+impute_mode <- function(x){
+  modal_value <- as.numeric(names(table(sort(x), decreasing = TRUE[1]))) ##Categorical data
+  replace(x, is.na(x),modal_value)
+}
+
+##Calling the function to remove missing values
+impute_mean(data$depth)
+impute_mode(data$color)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ###Exploring the data(EDA)
 
 view(df)
