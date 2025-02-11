2706246ad8c5753c2427a6f29c75ebd77b0e39e9
lynn-sketch <110109942+lynn-sketch@users.noreply.github.com>
2025-02-11 16:21:04 +0300

    Add files via upload

---
+++ WorldBankAssignment.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+library(tidyselect)
+library(readxl)
+library(readr)
+library(ggplot2)
+library(dplyr)
+library(data.table)
+
+setwd("C:/Users/HP PC/Desktop/BSDS 2_2/BIG DATA ANALYTICS/PracticeExercises for R")
